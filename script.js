894046f01f582604a0d0714c4ffd31a8732b7f5c
Hater <mymac@MacBook-Pro-admin.local>
2019-08-12 22:06:28 +0300

    script2

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
-const age = 21;
+// // // const age = 21;
 
-let counter = 1;    
+// // // let counter = 1;    
 
-// while (counter < 1000) {
-//     if (counter % age === 0) {
-//         console.log(`counter -  ${counter}`);
+// // // while (counter < 1000) {
+// // //     if (counter % age === 0) {
+// // //         console.log(`counter -  ${counter}`);
+// // //     }
+// // //     counter++;
+// // // }
+
+// // // console.log('do while');
+
+
+// // // do {
+// // //     if (counter % age === 0) {
+// // //         console.log('counter - ', counter);
+// // //     }
+// // //     counter++;
+// // // } while (counter < 1000);
+
+// // // for (let counter = 1; counter < 1000; counter++) {
+// // //     if (counter % age === 0) {
+// // //         console.log('counter - ', counter);
+// // //     }
+// // // }
+
+
+// // function areaFunction(side, h) {
+// //     const area = (side * h) / 2;
+// //     return area;
+// // }
+
+// // // console.log(areaFunction(3,4));
+
+// // function showArea () {
+// //     const otherArea = areaFunction(3, 4);
+// //     console.log('your mama is too big LoL -', otherArea);
+// // }
+
+// // showArea();
+
+
+
+
+
+
+
+// function sumFromTo(from, to) {
+//     if (from && to && !isNaN(from) && !isNaN(to)) {
+//         let sum = 0;
+//         for (let counter = from; counter <= to; counter++) {
+//             sum += counter;
+//         }
+//         return sum;
+//     } else {
+//         return 'someshit';
 //     }
-//     counter++;
 // }
 
-console.log('do while');
+// console.log(sumFromTo());
+// console.log(sumFromTo(3, 9));
+// console.log(sumFromTo(3, 9));
 
 
-do {
-    if (counter % age === 0) {
-        console.log('counter - ', counter);
-    }
-    counter++;
-} while (counter < 1000);
 
-for (let counter = 1; counter < 1000; counter++) {
-    if (counter % age === 0) {
-        console.log('counter - ', counter);
+
+
+
+
+function createList(list) {
+    for (let counter = 0; counter < list.length; counter++) {
+        const firstLetter = list[counter][0].toUpperCase();
+        console.log(firstLetter + list[counter].substr(1));
+
     }
 }
+
+const secondList = ['Beer', 'Creeps', 'Hodld'];
+
+createList(secondList);
